14798e86958e505145d592ff33042c106bb24008
Kathleen Mercado <kathleenmerc@gmail.com>
2022-11-22 18:02:50 -0500

    created .env, installed mongoose, connected to mongoDB, created a Schema in pokemon.js, updated post route to create Pokemon, updated Index route

--- models/pokemon.js
+++ models/pokemon.js
@@ -1,12 +1,36 @@
-const pokemon = [
-    {name: "bulbasaur", img: "http://img.pokemondb.net/artwork/bulbasaur"},
-    {name: "ivysaur", img: "http://img.pokemondb.net/artwork/ivysaur"},
-    {name: "venusaur", img: "http://img.pokemondb.net/artwork/venusaur"},
-    {name: "charmander", img: "http://img.pokemondb.net/artwork/charmander"},
-    {name: "charizard", img: "http://img.pokemondb.net/artwork/charizard"},
-    {name: "squirtle", img: "http://img.pokemondb.net/artwork/squirtle"},
-    {name: "wartortle", img: "http://img.pokemondb.net/artwork/wartortle"}
- ];
+const mongoose = require('mongoose')
+
+const pokemonSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    img: { data: Buffer, type: String }
+},
+{
+    timestamps: true
+})
 
 
- module.exports = pokemon
+const Pokemon = mongoose.model('Pokemon', pokemonSchema)
+
+module.exports = Pokemon
+
+
+
+
+
+
+
+
+
+// THIS IS DATA BEFORE CONNECTING TO MongoDB:
+
+// const pokemon = [
+//     {name: "bulbasaur", img: "http://img.pokemondb.net/artwork/bulbasaur"},
+//     {name: "ivysaur", img: "http://img.pokemondb.net/artwork/ivysaur"},
+//     {name: "venusaur", img: "http://img.pokemondb.net/artwork/venusaur"},
+//     {name: "charmander", img: "http://img.pokemondb.net/artwork/charmander"},
+//     {name: "charizard", img: "http://img.pokemondb.net/artwork/charizard"},
+//     {name: "squirtle", img: "http://img.pokemondb.net/artwork/squirtle"},
+//     {name: "wartortle", img: "http://img.pokemondb.net/artwork/wartortle"}
+//  ];
+
+//  module.exports = pokemon
--- server.js
+++ server.js
@@ -1,8 +1,20 @@
+require('dotenv').config()
+
 const express = require("express")
 const app = express()
 const PORT = 3000
-const pokemon = require("./models/pokemon")
+const Pokemon = require("./models/pokemon")
 const reactViews = require("express-react-views")
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+mongoose.connection.once("open", () => {
+    console.log("Connected to mongoDB")
+})
 
 app.set("view engine", "jsx")
 app.engine("jsx", reactViews.createEngine())
@@ -12,6 +24,7 @@ app.use((req, res, next)=> {
     next()
 })
 
+// Returns middleware that only parses URL-encoded data
 app.use(express.urlencoded({extended:false}));
 
 //HOMEPAGE
@@ -21,8 +34,13 @@ app.get("/", (req, res) => {
 
 //INDEX
 app.get("/pokemon", (req, res) => {
-    //res.send(pokemon)
-    res.render("Index", pokemon)
+    Pokemon.find({}, (error, allPokemon) => {
+        if (!error) {
+            res.status(200).render("Index", { pokemon: allPokemon })
+        } else {
+            res.status(400).send(error)
+        }
+    })
 })
 
 app.get("/pokemon/new", (req, res) => {
@@ -31,6 +49,15 @@ app.get("/pokemon/new", (req, res) => {
 
 //CREATE
 app.post("/pokemon", (req, res) => {
+    Pokemon.create(req.body, (error, createdPokemon) => {
+        if (!error) {
+            console.log(createdPokemon)
+            res.status(200).redirect("/pokemon")
+        } else {
+            res.status(400).send(error)
+        }
+    })
+
     pokemon.push(req.body)
     res.redirect("/pokemon")
 })
